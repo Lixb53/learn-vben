e62688d5c39f83197be04d5a9f157e8e75a1c8de
Lixb53 <li946226@sina.com>
2021-08-17 14:35:43 +0800

    'init'

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <div class="viteport">{{ vitePort }}</div>
+  <div class="viteport1">{{ vitePort }}</div>
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+</template>
+
+<script lang="ts">
+  import { defineComponent, ref } from 'vue';
+  import HelloWorld from './components/HelloWorld.vue';
+
+  export default defineComponent({
+    name: 'App',
+    components: {
+      HelloWorld,
+    },
+    setup() {
+      console.log(import.meta);
+      let title: string = import.meta.env.VITE_GLOB_APP_TITLE as string;
+      document.title = title;
+      const vitePort = ref(import.meta.env.VITE_PORT);
+      return {
+        vitePort,
+      };
+    },
+  });
+</script>
+
+<style>
+  #app {
+    margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    text-align: center;
+  }
+
+  .viteport {
+    position: absolute;
+    height: 50px;
+  }
+
+  .viteport1 {
+    position: absolute;
+    height: 50px;
+  }
+</style>

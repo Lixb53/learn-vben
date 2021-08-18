2e11cbd2ec270dcf52ad7cf3d1d1f06d79ac7028
Lixb53 <li946226@sina.com>
2021-08-18 09:43:40 +0800

    style: 添加脚本命令并且统一文件格式

--- vite.config.ts
+++ vite.config.ts
@@ -5,3 +5,15 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   plugins: [vue()],
 });
+
+import path$1 from 'path';
+const fs$2 = require('fs');
+
+function _interopDefaultLegacy(e) {
+  return e && typeof e === 'object' && 'default' in e ? e['default'] : e;
+}
+
+const path__default = /*#__PURE__*/ _interopDefaultLegacy(path$1);
+const fs__default = /*#__PURE__*/ _interopDefaultLegacy(fs$2);
+
+console.log(path__default, fs__default);
